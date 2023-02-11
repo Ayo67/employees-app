7104a6d1cdeeb85f96b67bbf330b69c691ba862b
ayoog <20098135@mail.wit.ie>
2023-02-11 21:18:44 +0000

    Completed adding color scheme to menus

---
+++ build/kotlin/compileKotlin/cacheable/caches-jvm/lookups/lookups.tab.values.s
@@ -0,0 +1,1 @@
+äsçqãpÞrýoâqÌrÚw
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,8 +6,13 @@ Author     : Ayo Oguntuyi
 package ie.setu
 import controllers.EmployeeAPI
 import ie.setu.ConsoleColours.BLUE
+import ie.setu.ConsoleColours.GREEN
+import ie.setu.ConsoleColours.PURPLE
 import ie.setu.ConsoleColours.RED
+import ie.setu.ConsoleColours.RED_UNDERLINED
 import ie.setu.ConsoleColours.RESET
+import ie.setu.ConsoleColours.WHITE_UNDERLINED
+import ie.setu.ConsoleColours.YELLOW
 import models.Employee
 import mu.KotlinLogging
 
@@ -19,9 +24,6 @@ val logger = KotlinLogging.logger {}
 
 fun main(args: Array<String>){
     logger.info { "Launching Employee App" }
-    logger.info { "Launching Employee App" }
-    logger.info { "Launching Employee App" }
-    logger.info { "Launching Employee App" }
     start()
 }
 
@@ -32,20 +34,20 @@ var employees = EmployeeAPI()
 
 
 fun menu() : Int {
-    print(""" ${RED}
-         |Employee Menu ${RESET}
-         ${BLUE}
-         |   1. Add Employee
-         |   2. List All Employees
-         |   3. Search Employees 
-         |   4. Print Payslip for Employee
-         |   5. Delete an Employee
-         |   6. Update Employee details
-         |   7. List all employees earning under a certain amount
-         |   8. Sort by gender
-         |   0. Exit 
+    print(""" $RED_UNDERLINED
+         |Employee Menu $RESET
+         
+         $BLUE|   1. Add Employee$RESET
+         $YELLOW|   2. List All Employees$RESET
+         $GREEN|   3. Search Employees $RESET
+         $PURPLE|   4. Print Payslip for Employee$RESET
+         $BLUE|   5. Delete an Employee$RESET
+         $YELLOW|   6. Update Employee details$RESET
+         $GREEN|   7. List all employees earning under a certain amount$RESET
+         $PURPLE|   8. Sort by gender$RESET
+         $RED_UNDERLINED|   0. Exit $RESET
          |       
-         |Enter Option :${RESET} """.trimMargin())
+         |Enter Option : """.trimMargin())
     return readLine()!!.toInt()
 }
 
@@ -63,7 +65,11 @@ fun start() {
             6 -> update()
             7 -> listEmployeesUnderAmount()
             8 -> sortByGender()
-            0 -> println("Exiting App")
+            99 -> dummyData()
+            0 -> {
+                println("Exiting App")
+                logger.info("Exited Employee App")
+            }
             else -> println("Invalid Option")
         }
         println()
@@ -73,15 +79,30 @@ fun list() {
         val employees = employees.findAll()
         if (employees.isEmpty()) {
             println("No employees found.")
+            logger.info("No employees found.")
         } else {
             employees.forEach { println(it.toString()) }
         }
 }
 
+fun dummyData() {
+    employees.create(Employee("Joe", "Soap", 'm', 0, 35655.43, 31.0, 7.5, 2000.0, 25.6))
+    employees.create(Employee("Joan", "Murphy", 'f', 1, 54255.13, 32.5, 7.0, 1500.0, 55.3))
+    employees.create(Employee("Mary", "Quinn", 'f', 2, 75685.41, 40.0, 8.5, 4500.0, 0.0))
+    employees.create(Employee("Robert", "Downey", 'm', 3, 45000.00, 27.0, 6.0, 1000.0, 10.0))
+    employees.create(Employee("Jane", "Lavender", 'f', 4, 55000.00, 28.0, 7.0, 2000.0, 20.0))
+    employees.create(Employee("Jim", "Smith", 'm', 5, 65000.00, 35.0, 8.0, 3000.0, 30.0))
+    employees.create(Employee("Christopher", "Nolan", 'f', 6, 75000.00, 40.0, 9.0, 4000.0, 40.0))
+    employees.create(Employee("Jessica", "Brown", 'f', 7, 85000.00, 45.0, 10.0, 5000.0, 50.0))
+    logger.info("Dummy data added.")
+}
+
+
 fun search() {
     val employee = getEmployeeById()
     if (employee == null) {
         println("No employee found")
+        logger.info {"No employee found"}
     }else {
         println(employee)
     }
@@ -159,6 +180,7 @@ fun update() {
         val updatedEmployee = Employee(firstName, surname, gender, employeeID, grossSalary, payePercentage, prsiPercentage, annualBonus, cycleToWorkMonthlyDeduction)
         employees.update(employee, updatedEmployee)
         println("Employee ID:${employee.employeeID} was updated")
+        logger.info { "EmployeeID : ${employeeID}" }
     } else {
         println("No employee was found with the selected ID")
     }
