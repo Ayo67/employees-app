25f15c4d5d9c9e9e113f5956178b190d7bff7429
ayoog <20098135@mail.wit.ie>
2023-02-11 21:17:08 +0000

    Completed adding color scheme to menus

--- src/main/kotlin/ConsoleColours.kt
+++ src/main/kotlin/ConsoleColours.kt
@@ -1,5 +1,6 @@
 package ie.setu
 
+// Colour scheme taken from W3 schools website
 object ConsoleColours {
 
 
@@ -15,4 +16,21 @@ object ConsoleColours {
         const val PURPLE = "\u001b[35m"
         const val CYAN = "\u001b[36m"
         const val WHITE = "\u001b[37m"
-    }
+
+        const val BOLD =  "\u001b[1m"
+        const val UNDERLINE ="\u001b[4m"
+
+        const val BLACK_UNDERLINED = "\u001b[4;30m"
+        const val RED_UNDERLINED = "\u001b[4;31m"
+        const val GREEN_UNDERLINED = "\u001b[4;32m"
+        const val YELLOW_UNDERLINED = "\u001b[4;33m"
+        const val BLUE_UNDERLINED = "\u001b[4;34m"
+        const val PURPLE_UNDERLINED = "\u001b[4;35m"
+        const val CYAN_UNDERLINED = "\u001b[4;36m"
+        const val WHITE_UNDERLINED = "\u001b[4;37m"
+
+
+
+
+
+}
